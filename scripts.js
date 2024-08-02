c5cde393a0ca3a1579dac5ac6fdcdf44ae9517fd
JEY HIRANO <mrjr0722@me.com>
2024-08-02 22:13:04 +0900

    インクルードテスト

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 function toggleMenu() {
-    var navLinks = document.getElementById("nav-links");
-    if (navLinks.className === "nav-links") {
-        navLinks.className += " responsive";
-    } else {
-        navLinks.className = "nav-links";
-    }
+  var navLinks = document.getElementById("nav-links");
+  if (navLinks.className === "nav-links") {
+    navLinks.className += " responsive";
+  } else {
+    navLinks.className = "nav-links";
+  }
 }
 
 const burger = document.querySelector(".burger");
@@ -21,9 +21,8 @@ burger.addEventListener("click", () => {
       link.style.animation = "";
     } else {
       // console.log(index);
-      link.style.animation = `navLinksFade 0.5s ease forwards ${
-        index / 7 + 0.4
-      }s`;
+      link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.4
+        }s`;
     }
   });
   //burger animataion
@@ -32,8 +31,31 @@ burger.addEventListener("click", () => {
 
 //ハンバーガーメニューからリンクを押したら閉じる
 navLinks.forEach((link, index) => {
-  link.addEventListener("click", () => { 
+  link.addEventListener("click", () => {
     if (window.getComputedStyle(burger).display === 'none') return;
-    burger.click(); 
+    burger.click();
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // var copyButton = document.getElementById("copyButton");
+  // var linkText = document.getElementById("link").href;
+
+  // copyButton.addEventListener("click", function() {
+  //     navigator.clipboard.writeText(linkText).then(function() {
+  //         alert("リンクがコピーされました: " + linkText);
+  //     }, function(err) {
+  //         console.error("リンクのコピーに失敗しました:", err);
+  //     });
+  // });
+
+  copyButton.addEventListener("click", function () {
+    var url = location.href;
+    navigator.clipboard.writeText(url).then(function () {
+      alert("リンクがコピーされました: " + url);
+    }, function (err) {
+      console.error("リンクのコピーに失敗しました:", err);
+    });
+  });
+
 });
